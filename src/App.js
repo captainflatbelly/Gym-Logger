bc84d6a323fedc97bc8066b8c77f7e43a5481360
Nupur513 <128990611+Nupur513@users.noreply.github.com>
2024-05-20 18:10:29 +0530

    added cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Hero from "./components/Hero";
 import Feature1 from "./components/Feature1";
 import Feature2 from "./components/Feature2";
 import Feature3 from "./components/Feature3";
+import Cards from "./components/Cards";
+import Form from "./components/Form";
+
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
       <Feature1 />
       <Feature2 />
       <Feature3 />
+      <Cards />
+      <Form />
+     
+
     </div>
   );
 }
